718c9bcd8da53dfab55a33cdac04136c31cc775b
aparkening <aaron.parkening@gmail.com>
2019-12-04 12:39:19 -0500

    Clean up comments and improve AI strategy.

--- lib/board.rb
+++ lib/board.rb
@@ -19,12 +19,6 @@ class Board
       puts "-----------"
       index += 3
     end
-    
-    # puts " #{self.cells[0]} | #{self.cells[1]} | #{self.cells[2]} "
-    # puts "-----------"
-    # puts " #{self.cells[3]} | #{self.cells[4]} | #{self.cells[5]} "
-    # puts "-----------"
-    # puts " #{self.cells[6]} | #{self.cells[7]} | #{self.cells[8]} "
   end
 
   # Convert input to integer
